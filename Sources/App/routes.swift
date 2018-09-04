b2cc8c66bbcd63f29bd60c6ed70b87b1e9e59b67
boxue11 <11@boxue.io>
2018-09-04 13:45:56 +0800

    Add about route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -7,6 +7,10 @@ public func routes(_ router: Router) throws {
         return "Hello, world!"
     }
 
+    router.get("about") { req in
+        return "About us."
+    }
+
     // Example of configuring a controller
     let todoController = TodoController()
     router.get("todos", use: todoController.index)
